224d2044b23d98f8d01cda24cc7cdcb194bb4b75
Patrik Turemark <p.turemark@gmail.com>
2025-04-10 13:35:25 +0200

    fixed footer in mobile mode and put picutre in map

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ showBtn.addEventListener("click", function () {
   if (showMore === false) {
     showMore = true;
     experience.classList.remove("exp-hide");
-    //chang text on showBtn to "Show less"
+    //change text on showBtn to "Show less"
     showBtn.innerHTML = "Visa mindre";
   } else {
     showMore = false;
     experience.classList.add("exp-hide");
+    showBtn.innerHTML = "Visa mer";
   }
 });
