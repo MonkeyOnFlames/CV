d6049002a8e2f0f8051b40cd9064bd2f28108e51
Patrik Turemark <p.turemark@gmail.com>
2025-04-10 10:56:23 +0200

    lade till en knapp som visar mer eller mindra på erfarenthet

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ let showMore = false;
 const experience = document.getElementById("experience");
 const showBtn = document.getElementById("show-btn");
 
-document.addEventListener("click", function () {
-  event.preventDefault();
-
+showBtn.addEventListener("click", function () {
   if (showMore === false) {
     showMore = true;
     experience.classList.remove("exp-hide");
-    //showBtn
+    //chang text on showBtn to "Show less"
+    showBtn.innerHTML = "Visa mindre";
   } else {
     showMore = false;
     experience.classList.add("exp-hide");
